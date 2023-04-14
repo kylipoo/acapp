e8591e6c2580c69e5286bd916bf62188c99cc58f
kyle li <kyleyli@gmail.com>
2023-04-15 05:17:23 +0800

    create project structure

--- game/views.py
+++ game/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import openai
+import os
+os.environ['OPENAI_API_KEY']='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'
+openai.api_key=os.getenv('OPENAI_API_KEY')
+# response = openai.Completion.create(model='text-davinci-003',prompt='Give me two resons to learn OpenAI API with Python', max_tokens=300)
 def index(request):
-    return HttpResponse("my first page")
-# Create your views here.
+
+#    return HttpResponse(response['choices'][0]['text'])
+ return HttpResponse('My First Page!')
+# Create your views herei
