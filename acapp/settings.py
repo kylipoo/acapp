ebb09fbfdfea08390f0bca290587836e6491bf1e
kyle li <kyleyli@gmail.com>
2023-04-21 04:56:53 +0800

    nginx

--- acapp/settings.py
+++ acapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ls66!ql_#rcuz1x#dn9$))yn$vtwe_b6_lki4vz8*0$d719kew'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8.218.199.133"]
+ALLOWED_HOSTS = ["8.218.199.133", "app5257.acapp.acwing.com.cn"]
 
 
 # Application definition
---
+++ static/js/src/menu/zbase.js
@@ -0,0 +1,50 @@
+class AcGameMenu {
+    constructor(root) {
+        this.root = root;
+        this.$menu = $(`
+<div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
+            Single
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
+            Multiple
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings-mode">
+            Settings
+        </div>
+    </div>
+</div>
+            `);
+        this.root.$ac_game.append(this.$menu);
+        this.$single_mode=this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode=this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings_mode=this.$menu.find('.ac-game-menu-field-item-settings-mode');
+        this.start();
+    }
+    start() {
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        let outer = this;
+        this.$single_mode.click(() => {
+           outer.hide();
+            outer.root.playground.show();
+        });
+        this.$multi_mode.click(()=> {
+           console.log("click multi mode");
+        });
+        this.$settings_mode.click(()=> {
+            console.log("click settings");
+        });
+     }
+    show() {
+        this.$menu.show();
+    }
+    hide() {
+        this.$menu.hide();
+    }
+}
